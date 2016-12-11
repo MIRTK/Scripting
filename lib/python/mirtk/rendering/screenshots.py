2e05f7c5129bc7a6c354d51b42dd15698c05c359
Andreas Schuh <andreas.schuh.84@gmail.com>
2016-12-11 19:22:27 +0000

    enh: Extend take_orthogonal_screenshots function

--- lib/python/mirtk/rendering/screenshots.py
+++ lib/python/mirtk/rendering/screenshots.py
@@ -23,6 +23,24 @@ import os
 import vtk
 
 
+default_colors = [
+    (1, 0, 0),
+    (0, 0, 1),
+    (0, 1, 0),
+    (0, 1, 1),
+    (1, 0, 1),
+    (1, 1, 0),
+    (1, 1, 1)
+]
+
+
+def deep_copy(obj):
+    """Make deep copy of VTK object."""
+    copy = obj.NewInstance()
+    copy.DeepCopy(obj)
+    return copy
+
+
 def iround(x):
     """Round floating point number and cast to int."""
     return int(round(x))
@@ -38,8 +56,7 @@ def nearest_voxel(index):
 
 def invert_matrix(m):
     """Get inverse of a vtkMatrix4x4."""
-    inv = vtk.vtkMatrix4x4()
-    inv.DeepCopy(m)
+    inv = deep_copy(m)
     inv.Invert()
     return inv
 
@@ -103,13 +120,17 @@ def add_contour(renderer, plane, polydata,
         transformer.SetInputData(polydata)
         transformer.SetTransform(transform)
         transformer.Update()
-        polydata = transformer.GetOutput()
+        polydata = deep_copy(transformer.GetOutput())
+        transformer = None
     cutter = vtk.vtkCutter()
     cutter.SetInputData(polydata)
     cutter.SetCutFunction(plane)
+    cutter.Update()
+    contour = deep_copy(cutter.GetOutput())
+    cutter = None
 
     mapper = vtk.vtkPolyDataMapper()
-    mapper.SetInputConnection(cutter.GetOutputPort())
+    mapper.SetInputData(contour)
 
     actor = vtk.vtkActor()
     actor.SetMapper(mapper)
@@ -132,7 +153,30 @@ def add_contour(renderer, plane, polydata,
     return actor
 
 
-def slice_view(image, index, width, height, axis=2, polydata=[],
+def slice_axes(zdir):
+    """Get volume dimensions corresponding to x and z axes of slice."""
+    if zdir == 0:
+        return (1, 2)
+    elif zdir == 1:
+        return (0, 2)
+    elif zdir == 2:
+        return (0, 1)
+    else:
+        raise Exception("Invalid zdir argument: " + zdir)
+
+
+def cropping_region(center, axes, width, height):
+    bounds = [center[0], center[0], center[1], center[1], center[2], center[2]]
+    i = 2 * axes[0]
+    j = 2 * axes[1]
+    bounds[i] -= (width - 1) / 2
+    bounds[i + 1] += (width + 1) / 2
+    bounds[j] -= (height - 1) / 2
+    bounds[j + 1] += (height + 1) / 2
+    return bounds
+
+
+def slice_view(image, index, width, height, zdir=2, polydata=[], colors=default_colors,
                transform=None, line_width=3, level_window=None):
     """Return vtkRenderer for orthogonal image slice."""
 
@@ -140,7 +184,7 @@ def slice_view(image, index, width, height, axis=2, polydata=[],
     flip = [False, False, False]
     if transform:
         transform.Update()
-        matrix = transform.GetMatrix()
+        matrix = deep_copy(transform.GetMatrix())
         try:
             from nibabel import aff2axcodes
             codes = aff2axcodes(matrix_to_affine(matrix))
@@ -160,51 +204,29 @@ def slice_view(image, index, width, height, axis=2, polydata=[],
             flip[2] = True
 
     dims = image.GetDimensions()
-    if axis == 0:
-        axes = (1, 2, 0)
-        if width < 1:
-            width = dims[1]
-        if height < 1:
-            height = dims[2]
-        size = (1, width, height)
-        up = (0, 0, 1)
-    elif axis == 1:
-        axes = (0, 2, 1)
-        if width < 1:
-            width = dims[0]
-        if height < 1:
-            height = dims[2]
-        size = (width, 1, height)
-        up = (0, 0, 1)
-    elif axis == 2:
-        axes = (0, 1, 2)
-        if width < 1:
-            width = dims[0]
-        if height < 1:
-            height = dims[1]
-        size = (width, height, 1)
+    axes = slice_axes(zdir)
+    if width < 1:
+        width = dims[axes[0]]
+    if height < 1:
+        height = dims[axes[1]]
+    size = [1, 1, 1]
+    size[axes[0]] = width
+    size[axes[1]] = height
+    if zdir == 2:
         up = (0, 1, 0)
     else:
-        raise Exception("Invalid axis argument: {}".format(axis))
-
+        up = (0, 0, 1)
     spacing = image.GetSpacing()
-    distance = 10. * spacing[axis]
+    distance = 10. * spacing[zdir]
     focal_point = index_to_point(index, image.GetOrigin(), spacing)
     position = list(focal_point)
-    if flip[axis]:
-        position[axis] = position[axis] - distance
+    if flip[zdir]:
+        position[zdir] = position[zdir] - distance
     else:
-        position[axis] = position[axis] + distance
+        position[zdir] = position[zdir] + distance
 
     margin = 2
-    extent = [index[0], index[0],
-              index[1], index[1],
-              index[2], index[2]]
-    for i in range(3):
-        if i != axis:
-            radius = (size[i] - 1) / 2 + margin
-            extent[2 * i] -= radius
-            extent[2 * i + 1] += radius
+    extent = cropping_region(index, axes, size[axes[0]] + margin, size[axes[1]] + margin)
 
     if flip[0] or flip[1] or flip[2]:
         flip_transform = vtk.vtkTransform()
@@ -223,8 +245,8 @@ def slice_view(image, index, width, height, axis=2, polydata=[],
 
     mapper = vtk.vtkImageSliceMapper()
     mapper.SetInputData(image)
-    mapper.SetOrientation(axis)
-    mapper.SetSliceNumber(extent[2 * axis])
+    mapper.SetOrientation(zdir)
+    mapper.SetSliceNumber(extent[2 * zdir])
     mapper.SetCroppingRegion(extent)
     mapper.CroppingOn()
     mapper.Update()
@@ -250,28 +272,29 @@ def slice_view(image, index, width, height, axis=2, polydata=[],
     camera.SetFocalPoint(focal_point)
     camera.SetParallelScale(.5 * max((size[axes[0]] - 1) * spacing[axes[0]],
                                      (size[axes[1]] - 1) * spacing[axes[1]]))
-    camera.SetClippingRange(distance - .5 * spacing[axis],
-                            distance + .5 * spacing[axis])
+    camera.SetClippingRange(distance - .5 * spacing[zdir],
+                            distance + .5 * spacing[zdir])
     camera.ParallelProjectionOn()
 
     # add contours of polygonal data intersected by slice plane
-    colors = [
-        (1, 0, 0),
-        (0, 0, 1),
-        (0, 1, 0),
-        (0, 1, 1),
-        (1, 0, 1),
-        (1, 1, 0),
-        (1, 1, 1)
-    ]
     if isinstance(polydata, vtk.vtkPolyData):
         polydata = [polydata]
-    if len(polydata) > len(colors):
-        raise Exception('Maximum number of contour overlays is: {}'.format(len(colors)))
+    if isinstance(line_width, int):
+        line_width = [line_width]
+    if isinstance(colors[0], float):
+        colors = [colors]
     for i in xrange(len(polydata)):
+        if i < len(colors):
+            color = colors[i]
+        else:
+            color = colors[-1]
+        if i < len(line_width):
+            width = line_width[i]
+        else:
+            width = line_width[-1]
         add_contour(renderer, plane=mapper.GetSlicePlane(),
                     polydata=polydata[i], transform=points_transform,
-                    line_width=line_width, color=colors[i])
+                    line_width=width, color=color)
     return renderer
 
 
@@ -306,9 +329,13 @@ def take_screenshot(window, path=None):
         return Image(data)
 
 
-def take_orthogonal_screenshots(image, center, length=0, size=(512, 512),
-                                prefix='screenshot', delim='_',
-                                level_window=None, qform=None, polydata=[]):
+def take_orthogonal_screenshots(image, center=None, length=0, offsets=None,
+                                size=(512, 512), prefix='screenshot_{n}',
+                                suffix=('axial', 'coronal', 'sagittal'),
+                                path_format='{prefix}_{suffix}',
+                                level_window=None, qform=None,
+                                polydata=[], colors=[], line_width=3,
+                                trim=False, overwrite=True):
     """Take three orthogonal screenshots of the given image patch.
 
     Arguments
@@ -316,20 +343,51 @@ def take_orthogonal_screenshots(image, center, length=0, size=(512, 512),
 
     image : vtkImageData
         Volume data.
-    center : tuple
-        Coordinates of patch center point.
-    length : float
-        Side length of patch in mm.
-    size : list, tuple, int
-        Either int or 2-tuple/-list of screenshot image size.
+    center : (float, float, float) or (int, int, int)
+        vtkImageData coordinates or voxel indices (3-tuple of ints)
+        of patch center point. Be sure to pass tuple with correct type.
+        When not specified, the image center point is used.
+    length : float or int
+        Side length of patch either in mm (float) or number of voxels (int).
+    offsets : list of float or int, optional
+        One or more offsets in either mm (float) or number of voxels (int)
+        from the center point along the orthogonal viewing direction.
+        A screenshot of the volume of interest is taken for each combination
+        of offset and orthgonal viewing direction, i.e., the number of
+        screenshots is `3 * len(offset)`. For example, when the volume of
+        interest is the entire image, i.e., `center=None` and `length=0`,
+        this can be used to render multiple slices with one call of this function.
+    size : (int, int)
+        Either int or 2-tuple/-list of int values specifying the width and height of the screenshot.
     prefix : str
         Common output path prefix of screenshot files.
-    delim : str
-        Delimiting string between file name prefix,
-        patch center index, and orthogonal view suffix.
+    suffix : (str, str, str)
+        List or tuple of three strings used as suffix of the screenshot
+        take from the respective orthogonal view. The order is:
+        axial, coronal, sagittal.
+    path_format: str
+        A format string used to construct the file name of each individual screenshot.
+        Make sure to use a suitable format string such that every screenshot has a
+        unique file name. The allowed placeholders are:
+        - `{prefix}`: Substituted by the `prefix` argument.
+        - `{suffix}`: Substituted by the `suffix` argument corresponding to the orthogonal view.
+        - `{n}`: The 1-based index of the screenshot.
+        - `{i}`: The i (int) volume index of the screenshot center point before trimming.
+        - `{j}`: The j (int) volume index of the screenshot center point before trimming.
+        - `{k}`: The k (int) volume index of the screenshot center point before trimming.
+        - `{x}`: The x (float) vtkImageData coordinates of the center point before trimming.
+        - `{y}`: The y (float) vtkImageData coordinates of the center point before trimming.
+        - `{z}`: The z (float) vtkImageData coordinates of the center point before trimming.
+        - `{vi}`: The i (int) volume index of the viewport center point after trimming.
+        - `{vj}`: The j (int) volume index of the viewport center point after trimming.
+        - `{vk}`: The k (int) volume index of the viewport center point after trimming.
+        - `{vx}`: The x (float) vtkImageData coordinates of the viewport center point after trimming.
+        - `{vy}`: The y (float) vtkImageData coordinates of the viewport center point after trimming.
+        - `{vz}`: The z (float) vtkImageData coordinates of the viewport center point after trimming.
     qform : vtkMatrix4x4, optional
-        Homogeneous image world to vtkImageData coordinates transformation.
-    level_window : tuple, list, optional
+        Homogeneous vtkImageData coordinates to world transformation matrix.
+        For example, pass the vtkNIFTIImageReader.GetQFormMatrix().
+    level_window : (float, float), optional
         2-tuple/-list of level and window color transfer function parameters.
         When not specified, the auto_level_window function with default percentiles
         is used to compute an intensity range that is robust to outliers.
@@ -338,19 +396,59 @@ def take_orthogonal_screenshots(image, center, length=0, size=(512, 512),
         image slice plane and the contours rendered over the image.
         When a `qform` matrix is given, the points are transformed
         to image coordinates using the inverse of the `qform` matrix.
+    colors : list, optional
+        List of colors (3-tuples of float RGB values in [0, 1]) to use for each `polydata` contour.
+    trim : bool, optional
+        Whether to trim cropping region such that screenshot is contained within image bounds.
+    overwrite : bool, optional
+        Whether to overwrite existing output files. When this option is False,
+        a screenshot is only taken when the output file does not exist.
 
     Returns
     -------
 
-    Nothing
+    paths: list
+        List of 6-tuples with parameters used to render each screenshot and the
+        absolute file path of the written PNG image files. The number of screenshots
+        is divisable by three, where the first `len(paths)/3` screenshots are taken from
+        axial image slices, the next `len(paths)/3` screenshots are taken from coronal
+        slices, and the last `len(paths)/3` screenshots are taken from sagittal slices.
+
+        For each screenshot, the returned 6-tuple contains the following values:
+        - path:   Absolute path of PNG file.
+        - zdir:   Volume dimension corresponding to viewing direction.
+        - center: 3-tuple of screenshot center voxel indices before trimming.
+        - origin: 3-tuple of viewport center voxel indices after trimming.
+        - size:   2-tuple of extracted slice width and height.
+        - isnew:  Whether image file was newly written or existed already.
+                  This value is always True when `overwrite=True`.
 
     """
     if isinstance(size, int):
         size = (size, size)
     if not level_window:
         level_window = auto_level_window(image)
+    if not suffix or len(suffix) != 3:
+        raise Exception("suffix must be a tuple/list of three elements")
+    suffix = list(suffix)
+    suffix.reverse()
+    origin = image.GetOrigin()
     spacing = image.GetSpacing()
-    index = nearest_voxel(point_to_index(center, image.GetOrigin(), spacing))
+    dims = image.GetDimensions()
+    if os.path.splitext(path_format)[1] != '.png':
+        path_format += '.png'
+    if center and (isinstance(center[0], int) and
+                   isinstance(center[1], int) and
+                   isinstance(center[2], int)):
+        index = center
+    else:
+        if not center:
+            center = image.GetCenter()
+        index = nearest_voxel(point_to_index(center, origin, spacing))
+    if not offsets:
+        offsets = [0]
+    elif isinstance(offsets, (int, float)):
+        offsets = [offsets]
     if qform:
         linear_transform = vtk.vtkMatrixToLinearTransform()
         linear_transform.SetInput(invert_matrix(qform))
@@ -360,24 +458,70 @@ def take_orthogonal_screenshots(image, center, length=0, size=(512, 512),
     args = dict(
         polydata=polydata,
         transform=linear_transform,
-        level_window=level_window
+        level_window=level_window,
+        colors=colors,
+        line_width=line_width
     )
-    infix = ','.join(['{:03d}'.format(idx) for idx in index])
-    suffix = ('3-sagittal', '2-coronal', '1-axial')
-    directory = os.path.dirname(prefix)
-    if not os.path.isdir(directory):
-        os.makedirs(directory)
-    for i in xrange(3):
-        j = (i + 1) % 3
-        axis = (i + 2) % 3
-        width = iround(length / spacing[i])
-        height = iround(length / spacing[j])
-        if width % 2 == 0:
-            width += 1
-        if height % 2 == 0:
-            height += 1
-        renderer = slice_view(image, axis=axis, index=index, width=width, height=height, **args)
-        window = vtk.vtkRenderWindow()
-        window.SetSize(size)
-        window.AddRenderer(renderer)
-        take_screenshot(window, path=delim.join([prefix, infix, suffix[axis]]))
+    n = 0
+    screenshots = []
+    for zdir in (2, 1, 0):
+        xdir, ydir = slice_axes(zdir)
+        if isinstance(length, int):
+            width = length
+            height = length
+        else:
+            width = iround(length / spacing[xdir])
+            height = iround(length / spacing[ydir])
+        for offset in offsets:
+            ijk = list(index)
+            if isinstance(offset, int):
+                ijk[zdir] += offset
+            else:
+                ijk[zdir] += iround(offset / spacing[zdir])
+            vox = list(ijk)
+            if trim:
+                bounds = cropping_region(vox, (xdir, ydir, zdir), width, height)
+                if bounds[0] < 0:
+                    bounds[0] = 0
+                if bounds[1] >= dims[0]:
+                    bounds[1] = dims[0] - 1
+                if bounds[2] < 0:
+                    bounds[2] = 0
+                if bounds[3] >= dims[1]:
+                    bounds[3] = dims[1] - 1
+                if bounds[4] < 0:
+                    bounds[4] = 0
+                if bounds[5] >= dims[2]:
+                    bounds[5] = dims[2] - 1
+                offset = 2 * xdir
+                width = (bounds[offset + 1] - bounds[offset])
+                vox[xdir] = bounds[offset] + width / 2
+                offset = 2 * ydir
+                height = (bounds[offset + 1] - bounds[offset])
+                vox[ydir] = bounds[offset] + height / 2
+            renderer = slice_view(image, zdir=zdir, index=vox, width=width, height=height, **args)
+            window = vtk.vtkRenderWindow()
+            window.SetSize(size)
+            window.AddRenderer(renderer)
+            n += 1
+            xyz = index_to_point(ijk, origin, spacing)
+            pos = index_to_point(vox, origin, spacing)
+            path = os.path.abspath(path_format.format(
+                n=n, prefix=prefix, suffix=suffix[zdir],
+                vi=vox[0], vj=vox[1], vk=vox[2],
+                vx=pos[0], vy=pos[1], vz=pos[2],
+                i=ijk[0], j=ijk[1], k=ijk[2],
+                x=xyz[0], y=xyz[1], z=xyz[2],
+            ))
+            if overwrite or not os.path.isfile(path):
+                directory = os.path.dirname(path)
+                if not os.path.isdir(directory):
+                    os.makedirs(directory)
+                take_screenshot(window, path=path)
+                isnew = True
+            else:
+                isnew = False
+            screenshots.append((path, zdir, ijk, vox, (width, height), isnew))
+            renderer = None
+            window = None
+    return screenshots
